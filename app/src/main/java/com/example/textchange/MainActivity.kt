b68b9ba2492ab6760c8c4d5e186db6ccf1bb3338
vaishnavipatil07 <107480849+vaishnavipatil07@users.noreply.github.com>
2025-03-04 01:57:37 +0530

    Commit

---
+++ app/src/main/java/com/example/textchange/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.textchange
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = "Thank You For Visit to My Websiite!"
+        }
+    }
+}
